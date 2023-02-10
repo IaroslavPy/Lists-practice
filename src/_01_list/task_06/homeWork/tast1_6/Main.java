7ecf2a2e400b5517fd00a885ab2c76d9769c750b
IaroslavPy <pilipchuckyaroslav@gmail.com>
2023-02-10 18:04:58 +0200

    Add task1_6

---
+++ src/_01_list/task_06/homeWork/tast1_6/Main.java
@@ -0,0 +1,57 @@
+package _01_list.task_06.homeWork.tast1_6;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        output(getData());
+        checkData(getData());
+
+    }
+
+    private static List<String> getData() {
+        List<String> products = new LinkedList<>();
+        products.add("Butter");
+        products.add("Juice");
+        products.add("Meat");
+        products.add("Fish");
+        products.add("Sugar");
+        return products;
+    }
+
+    //   private static void checkData(List<String> dataList){
+//       String checkingProduct = "Mea";
+//       int counter = 0;
+//       for (String product: dataList){
+//           if (product.equals(checkingProduct)){
+//               counter++;
+//           }
+//       }
+//       if (counter != 0){
+//           System.out.println("\n" + checkingProduct + " found in the list!");
+//       } else {
+//           System.out.println("\n" + checkingProduct + " not found in the list!");
+//       }
+//   }
+    private static void checkData(List<String> dataList) {
+        String checkingProduct = "Met";
+        boolean hasProduct;
+        hasProduct = dataList.contains(checkingProduct);
+        if (hasProduct) {
+            System.out.println("\n" + checkingProduct + " found in the list!");
+        } else {
+            System.out.println("\n" + checkingProduct + " not found in the list!");
+        }
+
+    }
+
+    private static void output(List<String> outputList) {
+        System.out.println("\n Output: \n");
+        int counter = 1;
+        for (String product : outputList) {
+            System.out.println(counter + ". " + product + ".");
+            counter++;
+        }
+    }
+}
